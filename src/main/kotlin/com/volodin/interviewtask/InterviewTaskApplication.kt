707c49eed7ca3f5895311e62504a1dec1b47e803
earlout <volodinvladislav@gmail.com>
2020-10-05 13:40:38 +0200

    Initial commit

---
+++ src/main/kotlin/com/volodin/interviewtask/InterviewTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.volodin.interviewtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterviewTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/volodin/interviewtask/InterviewTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.volodin.interviewtask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterviewTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
